4fb0d23fc2a448ddb8c5e60c8e5df1bcf27ae6db
justinwoo <moomoowoo@gmail.com>
2017-03-25 23:36:25 +0200

    update 0.11

--- src/Data/Foreign/Generic.purs
+++ src/Data/Foreign/Generic.purs
@@ -19,17 +19,17 @@ defaultOptions =
   }
 
 -- | Read a value which has a `Generic` type.
-readGeneric :: forall a rep. (Generic a rep, GenericDecode rep) => Options -> Foreign -> F a
+readGeneric :: forall a rep. Generic a rep => GenericDecode rep => Options -> Foreign -> F a
 readGeneric opts = map to <<< decodeOpts opts
 
 -- | Generate a `Foreign` value compatible with the `readGeneric` function.
-toForeignGeneric :: forall a rep. (Generic a rep, GenericEncode rep) => Options -> a -> Foreign
+toForeignGeneric :: forall a rep. Generic a rep => GenericEncode rep => Options -> a -> Foreign
 toForeignGeneric opts = encodeOpts opts <<< from
 
 -- | Read a value which has a `Generic` type from a JSON String
-readJSONGeneric :: forall a rep. (Generic a rep, GenericDecode rep) => Options -> String -> F a
+readJSONGeneric :: forall a rep. Generic a rep => GenericDecode rep => Options -> String -> F a
 readJSONGeneric opts = parseJSON >=> readGeneric opts
 
 -- | Write a value which has a `Generic` type as a JSON String
-toJSONGeneric :: forall a rep. (Generic a rep, GenericEncode rep) => Options -> a -> String
+toJSONGeneric :: forall a rep. Generic a rep => GenericEncode rep => Options -> a -> String
 toJSONGeneric opts = toForeignGeneric opts >>> unsafeStringify
